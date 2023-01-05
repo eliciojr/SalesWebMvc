947e1132ddf3eafc05e3121e337d270b68bd4a28
eliciojr <eliciojr@gmail.com>
2023-01-04 21:47:10 -0300

    Projeto SalesWebMvc

---
+++ Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class ProductService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public ProductService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+    }
+}
